9de6112d0b9c7cf4b8918b5023470d03f4a28acd
= <=>
2022-06-30 16:11:02 -0700

    Done!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	/* This is the entry point into our program */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
