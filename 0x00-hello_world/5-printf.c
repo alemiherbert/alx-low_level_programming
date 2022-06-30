b4558bbf9c94162fe079e37563047c4cce481d72
= <=>
2022-06-30 16:26:27 -0700

    Done!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * main - the entry point into the program.
+ * this program demostrates the use of the printf function.
+ * Return: the program returns an integer 0, for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
