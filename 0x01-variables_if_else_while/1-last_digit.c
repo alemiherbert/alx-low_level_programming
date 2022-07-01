2dd6713523d1fc095b30933a6d1ce1af441f7c79
= <=>
2022-07-01 10:44:25 -0700

    Done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	if ((int)last_digit == 0)
 	{
-		printf("Last digit of %d is 0\n", n); 
+		printf("Last digit of %d is 0 and is 0\n", n); 
 	}
 
 	return (0);
