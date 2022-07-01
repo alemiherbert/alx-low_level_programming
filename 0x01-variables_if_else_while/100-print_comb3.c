d6b4a3c0f2a6147c6258807082b163f3a392b333
= <=>
2022-07-01 13:42:41 -0700

    Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,21 +3,24 @@
 
 /**
  * main - entry point
- * return 0 if success
+ * Return: 0 if success
  */
 int main(void)
 {
 	int i, j;
 	for (i = 48; i < 57; i++)
-	{;
+	{
 		for (j = 49; j <= 57; j++)
 		{
 			if (!(i >= j))
 			{
 				putchar(i);
 				putchar(j);
-				putchar(44);
-				putchar(32);
+				if (!(i == 56 && j == 57))
+				{
+					putchar(44);
+					putchar(32);
+				}
 			}
 		}
 	}
