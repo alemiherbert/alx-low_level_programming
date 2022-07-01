d469584132a799adf6dc27085a0e94d506640d16
= <=>
2022-07-01 12:11:30 -0700

    Done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - entry point into the program
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(44);
+			putchar(32);
+		c++;
+	}
+	putchar(10);
+	return (0);
+}
