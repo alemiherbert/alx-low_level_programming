809dc5e65287427588a5a227e5e150a995a0c79e
= <=>
2022-07-01 14:01:59 -0700

    Done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+/**
+ * main - entry point
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			for (k = 50; k <= 57; k++)
+			{
+				if (j > i && k > j)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == 55 & j == 56 && k == 57))
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}		
+	putchar(10);
+	return (0);
+}
