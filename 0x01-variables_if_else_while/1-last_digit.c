1fcd282b63b7318798b7157c5c203bf9cf428371
= <=>
2022-07-01 10:47:49 -0700

    Done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,7 +8,7 @@
  * (a) greater than 5
  * (b) 0
  * (c) less than 6 and 0
- * Return 0 for success
+ * Return: 0 for success
  */
 int main(void)
 {
@@ -21,7 +21,8 @@ int main(void)
 	if (last_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	if (last_digit < 6 && last_digit != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 \
+	and not 0\n", n, last_digit);
 	if ((int)last_digit == 0)
 	{
 		printf("Last digit of %d is 0 and is 0\n", n); 
