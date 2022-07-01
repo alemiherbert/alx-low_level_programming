a8f32442d3a0daf078bbef5352dc364788847258
= <=>
2022-07-01 11:49:19 -0700

    Done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ * main - entry point into the program
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int c = 97;
+
+	while (c <= 122)
+	{
+		if !(c == 101 || c == 113)
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar(10);
+	return (0);
+}
