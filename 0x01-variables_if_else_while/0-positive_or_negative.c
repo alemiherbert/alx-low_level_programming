c8f16511e2c66f5ae0816c546867c28328e25e6e
= <=>
2022-07-01 03:13:28 -0700

    all done!

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point  into the program
+ * Description: asign a random variable to a number n 
+ * and print wether the number is positive or negative
+ * Return: 0 for success */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}else
+	{
+		printf("%d is negative\n", n);
+	}
+	
+	
+	return (0);
+}
