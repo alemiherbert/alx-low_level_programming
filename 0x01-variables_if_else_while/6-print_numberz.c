43e8b103b90b6e58a6b308e549ff68a2f4437ce9
= <=>
2022-07-01 11:55:58 -0700

    Done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - entry point into the program
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int c = 30;
+
+	while (c <= 39)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(10);
+	return (0);
+}
