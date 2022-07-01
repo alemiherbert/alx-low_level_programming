7521ab5a9a2a7afe0c8f07e7ccc7584323101b90
= <=>
2022-07-01 10:58:37 -0700

    Done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,19 +12,19 @@
  */
 int main(void)
 {
-	int n, last_digit;
+	int n, t;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	last_digit = n % 10;
-	if (last_digit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	if (last_digit < 6 && last_digit != 0)
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
-	if ((int)last_digit == 0)
+	t = n % 10;
+	if (t > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, t);
+	if (t < 6 && t != 0)
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, t);
+	if ((int)t == 0)
 	{
-		printf("Last digit of %d is 0 and is 0\n", n); 
+		printf("Last digit of %d is 0 and is 0\n", n);
 	}
 	return (0);
 }
