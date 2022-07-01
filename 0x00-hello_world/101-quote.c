b9a2574926fb5337d919d0e63483627313555a6a
= <=>
2022-07-01 00:25:22 -0700

    Done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - entry point into our program
  * Description: uses the unistd library instead of stdio
- * Return: 0 for success
+ * Return: 1 for success
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
