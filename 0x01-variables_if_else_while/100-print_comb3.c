bf6b5a5a8d24fd6fb298f0f0117272254de3e9c8
= <=>
2022-07-01 13:35:02 -0700

    Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,18 @@
 int main(void)
 {
 	for (int i = 48; i < 57; i++)
-	{
+	{;
 		for (int j = 49; j <= 57; j++)
 		{
-			putchar(i);
-			putchar(j);
-			j++;
+			if (!(i >= j))
+			{
+				putchar(i);
+				putchar(j);
+				putchar(44);
+				putchar(32);
+			}
 		}
-		putchar(44);
-		putchar(32);
-		i++;
 	}
+	putchar(10);
 	return (0);
 }
