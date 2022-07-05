ba4967fe9f5dbb9f2c4fdabc24049dda917e453f
= <=>
2022-07-05 15:55:16 -0700

    Done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_alpbhabet_x10(void);
+{
+	int i;
+	char c;
+
+	for(i = 0; i < 10; i++)
+	{
+		for(c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c)
+		}
+		_putchar(c);
+	}
+}
