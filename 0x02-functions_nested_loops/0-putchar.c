6ad8bdd9ac2344993ab8552b61e2226540216fcb
= <=>
2022-07-05 14:38:07 -0700

    Completed task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - this is the entry point into the program
+ * Description: None
+ * Return: 1 on success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return(0);
+}
