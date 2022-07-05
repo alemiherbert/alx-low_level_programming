b100bae9560ba489dc160bcbfc8885f0436ffa3a
= <=>
2022-07-05 14:55:59 -0700

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all the lowercase letterd of the alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i = 97;
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	return;
+}	
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
