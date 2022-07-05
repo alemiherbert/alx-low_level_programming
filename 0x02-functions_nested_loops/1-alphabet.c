11b847f16755311b955d7245bb8a861d8fa33aa0
= <=>
2022-07-05 15:22:18 -0700

    Done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,13 @@
 #include "main.h"
 
-void print_aphabet(void);
-/**
- * print_alphabet - prints all the lowercase letterd of the alphabet
- *
- * Return: void
- */
 void print_alphabet(void)
 {
-        int i = 97;
-  
-  	while (i <= 122)
-        {
-                _putchar(i);
-                i++;
-        }
-  
-  	_putchar(10);
-        return;
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+
+	_putchar('\n');
 }
