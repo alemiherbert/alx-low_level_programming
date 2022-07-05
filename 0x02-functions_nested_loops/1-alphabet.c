151cbe021f4a19583422dd18a20cba2518cc2170
= <=>
2022-07-05 15:07:45 -0700

    Multiple edits

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,13 @@
 void print_alphabet(void)
 {
         int i = 97;
-        while (i <= 122)
+  
+  	while (i <= 122)
         {
                 _putchar(i);
                 i++;
         }
-        _putchar(10);
+  
+  	_putchar(10);
         return;
 }
