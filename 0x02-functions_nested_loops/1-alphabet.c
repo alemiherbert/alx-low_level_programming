06327f8b35b6e522419f3fcb3aa142411796b08a
= <=>
2022-07-05 15:45:10 -0700

    Done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,1 @@
-#include "main.h"
 
-/**
- * print_alphabet - prints lowercase letters
- * Return: nothing
- */
-void print_alphabet(void)
-{
-	char a;
-
-	for (a = 'a'; a <= 'z'; a++)
-	{
-		_putchar(a);
-	}
-
-	_putchar('\n');
-}
