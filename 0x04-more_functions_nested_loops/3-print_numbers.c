9cb6e1c40ff73364fd5bb558294f2a36b2377b21
= <=>
2022-07-08 16:18:44 -0700

    Done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
-
-
 /**
- * print_numbers - print the numbers
- *
- * Return: Nothing
- */
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
 void print_numbers(void)
 {
-	char c = '0';
-	for (; c < '9'; c++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
 }
