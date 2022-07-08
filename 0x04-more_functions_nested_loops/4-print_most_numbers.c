c2a002ad9cf5e76539138376385c442ce4bd6a73
= <=>
2022-07-08 16:21:23 -0700

    Done

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
-
 /**
- * print_numbers - print the numbers
- *
- * Return: Nothing
- */
-void print_numbers(void)
+*print_most_numbers - prints numbers except 2 and 4
+*
+*Return: returns nothing
+*/
+void print_most_numbers(void)
 {
-	char c = '0';
-	for (; c < '9'; c++)
-	{
-		if (c != '2' || c != '4')
-		{
-			_putchar(i);
-		}
-	}
-	_putchar('\n');
+int number;
+for (number = 48; number < 58; number++)
+{
+if ((number == 50) || (number == 52))
+{
+continue;
+}
+_putchar(number);
+}
+_putchar(10);
 }
