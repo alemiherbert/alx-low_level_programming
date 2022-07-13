614b2fa5ec9d93c7fdce7e79233b4359daf1400c
= <=>
2022-07-12 18:35:08 -0700

    Ready for checks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reversw
+ * @str: string to be printed;
+ */
+void print_rev(char *str)
+{
+	/* Get the length of the string */
+	int i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		str++;
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(*(str + 1));
+		str--;
+	}
+	_putchar('\n');
+}
