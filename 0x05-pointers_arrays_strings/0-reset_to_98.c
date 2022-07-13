575f87ee028694186b65b49462a0805f07fa045e
= <=>
2022-07-12 17:31:49 -0700

    Ready for checks

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - takes s pointer to an int and updates the value it points to 9
- * @*n: pointer to an int
+ * @n: pointer to an int
  * Return: None
  */
 void reset_to_98(int *n)
