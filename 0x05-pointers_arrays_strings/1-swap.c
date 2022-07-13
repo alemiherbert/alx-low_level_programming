06e8c07f001d70e61ec29f1072f59a7edd77f641
= <=>
2022-07-12 17:38:06 -0700

    Ready for checks

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of twon integers
+ * @a: the location of variable a;
+ * @b: the location of variable b;
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+	
