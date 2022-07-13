469e8310cc953d98287d3e8a1eb0f47eeece2c34
= <=>
2022-07-12 17:23:30 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes s pointer to an int and updates the value it points to 9
+ * @*n: pointer to an int
+ * Return: None
+ */
+void reset_to_98(int *n)
+{
+	n = 98;
+}
