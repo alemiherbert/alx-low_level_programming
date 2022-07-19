76cc54bf1775e93751e74de7ba4d1cd7c6ff1c1b
= <=>
2022-07-19 16:09:38 -0700

    Third file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search
+ * @c: character to locate
+ *
+ * Return: pointer to the first occurence of character or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		s++;
+		if (*s == c)
+			return (s);
+		else
+			return (NULL);
+	}
+}
