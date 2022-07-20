55b9f248b9653e14fb757f1843116d73e58a4279
= <=>
2022-07-20 12:33:34 -0700

    Bug fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		s++;
+		_putchar('\n');
+		return;
 	}
-	_puts_recursion((s + 1));
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
