d7ef84765b97f5a8c0d5a74bd06d00576b0b368f
= <=>
2022-07-20 18:56:08 -0700

    Power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - get the power of a number
+ * @x: base
+ * @y: power
+ * Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else 
+		return (x * factorial(x, y - 1));
+}
