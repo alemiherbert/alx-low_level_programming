72b7010978aafd16bb12052e5474a5f183cb2cd9
= <=>
2022-07-20 18:41:08 -0700

    Thirdd commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion -count the number of characters in a string
+ * @s: the string
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
+}
