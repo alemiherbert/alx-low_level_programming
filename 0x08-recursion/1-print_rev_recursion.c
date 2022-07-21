28e0001a363c7e8d15483c6e8472593ea6247eea
= <=>
2022-07-20 18:31:15 -0700

    Second commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.c"
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ *
+ * @s: string to print
+ *
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		return;
+}
