27f141316b224d99e79329dc8dfa308c00102ba9
= <=>
2022-07-22 11:23:48 -0700

    Dome

---
+++ 0x09-static_libraries/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		if (argc == 1)
+			printf("%s\n", argv[count]);
+		else
+			break;
+	}
+	return (0);
+}
