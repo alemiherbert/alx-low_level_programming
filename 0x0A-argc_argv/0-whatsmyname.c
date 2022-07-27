188486615fa90853d806e490df40f59556c4b4bd
= <=>
2022-07-27 15:07:16 -0700

    Done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ * @argc: no of arguments
+ * argv: argument vector
+ *
+ * Return: int
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		if (argc == 1)
+			printf("%s\n", argv[count]);
+		else
+			break
+	}
+	return (0);
+}
