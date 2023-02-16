d581a9fbb4f3c05cf05a3eeb60acccc4677f896b
alemi <alemiherbert@gmail.com>
2023-02-17 02:40:40 +0300

    Please betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point into our program
+ * Description: main is the function where all our code will live
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
