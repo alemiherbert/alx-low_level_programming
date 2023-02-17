789ca9b9138c55f0bd13036788ea776b02f432d4
alemi <alemiherbert@gmail.com>
2023-02-17 16:37:14 +0300

    Complete task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point into our program
+ * Description: This program prints all possible combinations of 3 digits
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char i, j, k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
