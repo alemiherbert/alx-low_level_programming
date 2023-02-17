e95793275f895e1902c5003cdbd212a249fa078f
alemi <alemiherbert@gmail.com>
2023-02-17 03:17:27 +0300

    Complete task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point into our program
+ * Description: main is the function where all our code will live
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
