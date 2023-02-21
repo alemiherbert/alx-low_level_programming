e32c037f4432023214ae144e49535f49b2f54dcf
alemi <alemiherbert@gmail.com>
2023-02-21 21:26:32 +0300

    Complete task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabetx10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabetx10 - print the alphabet in lowercase, 10 times
+ * Description: This function prints the alphabet 10 times
+ * it is followed by a newline
+ *
+ * Return:
+ */
+void print_alphabetx10(void)
+{
+	int i = 0;
+	char c = 'a';
+
+	while (i < 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
