b3c79c698c3a2b6d7216b63b32a5b4b2863c891e
alemi <alemiherbert@gmail.com>
2023-02-23 00:27:12 +0300

    Complete task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * main - the entry point into the program
+ * Description: thiis program tests my code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+# 0x03. C - Debugging
+In this chapter of the ALX experience, I explore the various techniques
+of debugging C code.
