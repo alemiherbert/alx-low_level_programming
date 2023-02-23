973e60a8b9f42615f49f1e7afdf951ecbf1ee46b
alemi <alemiherbert@gmail.com>
2023-02-24 01:04:27 +0300

    Complete task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,17 +14,11 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (c = 0; c < 15; c++)
-			{
-				if (c / 10 > 0)
-					_putchar((c / 10) + '0');
+		{
+			if (c / 10 > 0)
+				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
-			}
+		}
 		_putchar('\n');
 	}
-}
-
-int main()
-{
-	more_numbers();
-	return (0);
 }
