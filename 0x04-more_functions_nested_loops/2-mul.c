5120b916f6284610e82788c46293c4b2da9cadca
alemi <alemiherbert@gmail.com>
2023-02-24 00:26:48 +0300

    Complete task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers together
+ * @a: the first number
+ * @b: the second number
+ *
+ * Result: the product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
