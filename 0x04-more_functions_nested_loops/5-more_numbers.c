c800cea5604b504fe7a54646f33c418a5b460af5
alemi <alemiherbert@gmail.com>
2023-02-24 00:53:35 +0300

    Petty betty compliance

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,9 @@ void more_numbers(void)
 {
 	int i;
 	char c;
-	
+
 	for (i = 0; i < 10; i++)
-		for (c = 0; c <= 15; c++)
+		for (c = 0; c < 15; c++)
 		{
 			if (c / 10 > 0)
 				_putchar((c / 10) + '0');
