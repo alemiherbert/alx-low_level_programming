88401929d0aadc6f2bf9e5bcd1cafb439f27d7ac
alemi <alemiherbert@gmail.com>
2023-02-27 23:49:56 +0300

    Complete task 5

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,6 +6,8 @@ void swap_int(int *a, int *b);
 int _putchar(char c);
 int _strlen(char *s);
 void _puts(char *str);
-void _print_rev(char *s);
+void print_rev(char *s);
+void rev_string(char *s);
+
 
 #endif
