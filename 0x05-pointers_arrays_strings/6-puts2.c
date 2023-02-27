347c604178f7cef4129c6c6ac934158124e0df7c
alemi <alemiherbert@gmail.com>
2023-02-27 23:55:56 +0300

    Complete task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts2 - a function that prints every other character of a string to stdout
+ *
+ * @s: the string to print
+ *
+ * Return: 0 if all goes well
+ */
+int _puts2(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (i % 2 == 0)
+			_putchar(s[i]);
+	_putchar('\n');
+	return (0);
+}
