7320c6a09deee64c6c65bbfacf769ff9c6688b45
alemi <alemiherbert@gmail.com>
2023-03-02 01:00:13 +0300

    Complete task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * _strcmp - compare two strings
@@ -10,12 +10,10 @@ include "main.h"
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1)
+	while (*s1 && *s1 == *s2)
 	{
-		if (*s1 != *s2)
-			break;
 		s1++;
 		s2++;
 	}
-	return (*(char *)s1 - *(char *)s2);
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
