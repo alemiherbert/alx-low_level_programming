e953c42a24f394d3650b99ecdf5f2b458be94d5a
alemi <alemiherbert@gmail.com>
2023-03-03 00:43:23 +0300

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,44 @@
-#include <stdio.h>
-
-/**
- * cap_string - this function capitalises every word of a string
- * @s: the string to capitalise
+	/**
+ * cap_string - a function that capitalizes
+ *              all words of a string
+ *
+ * @s: pointer to char input array
  *
  * Return: the capitalised string
- */
+*/
+
 char *cap_string(char *s)
 {
-	int i, j;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	/*iterate through our array values*/
+	while (s[i] != '\0')
+	{
+		/*check for any lowercase letters*/
 		if (s[i] >= 97 && s[i] <= 122)
 		{
-			j = i - 1;
-			if (s[j] == 32 || s[j] == 09 || s[j] == 10 ||
-				s[j] == 44 || s[j] == 59 || s[j] == 46 ||
-				s[j] == 33 || s[j] == 63 || s[j] == 34 ||
-				s[j] == 40 || s[j] == 41 || s[j] == 123 ||
-				s[j] == 124)
+			/**
+			 * if we have a null character
+			 * change its value to capital
+			*/
+			if (i == 0)
+			{
 				s[i] -= 32;
-
+			}
+			/**
+			 * if we find any character matching the below before any small
+			 * letter we change that value to a capital letter.
+			*/
+			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
+				s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
+				s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
+				s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
+				s[i - 1] == 124)
+			{
+				s[i] -= 32;
+			}
 		}
+		i++;
+	}
 	return (s);
 }
