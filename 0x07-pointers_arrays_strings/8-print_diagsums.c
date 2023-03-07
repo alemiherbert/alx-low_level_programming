26e977be14da686f3b7bc9c8348f9d75f50ac0ec
alemi <alemiherbert@gmail.com>
2023-03-07 05:59:32 +0300

    Refactor

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,17 +8,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-int i, S1, S2;
+int i, s1, s2;
 
-	S1 = 0;
-	S2 = 0;
+	s1 = 0;
+	s2 = 0;
 
 	for (i = 0; i < (size * size); i++)
 	{
 		if (i % (size + 1) == 0)
-			S1 += a[i];
+			s1 += a[i];
 		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-			S2 += a[i];
+			s2 += a[i];
 	}
-	printf("%d, %d\n", S1, S2);
+	printf("%d, %d\n", s1, s2);
 }
