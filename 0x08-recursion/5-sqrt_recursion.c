aa69d76cb6100e9883882027656ac9d439659875
alemi <alemiherbert@gmail.com>
2023-03-08 20:11:29 +0300

    Complete task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - compute the square root of a number using recursion
+ * @n: the number
+ * @Return: the root if it is natural, else -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+	return 0
+}
