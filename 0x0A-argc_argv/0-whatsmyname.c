51983e6651c298c8832749192732940c838ff2f2
alemi <alemiherbert@gmail.com>
2023-03-11 04:20:14 +0300

    Complet task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-
+/**
+ * main - a program that prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
 int main(int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
