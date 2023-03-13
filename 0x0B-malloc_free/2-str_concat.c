163b59778c964d566751b92fb474dda8658f8ca7
alemi <alemiherbert@gmail.com>
2023-03-14 00:00:52 +0300

    Complete task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,11 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j;
 	char *result;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
@@ -22,19 +27,10 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
-	{
-		if (s1 == NULL)
-			break;
 		result[i] = s1[i];
-	}
-
 	j = i;
 	for (i = 0; s2[i] != '\0'; i++, j++)
-	{
-		if (s2 == NULL)
-			break;
 		result[j] = s2[i];
-	}
 
 	return (result);
 }
