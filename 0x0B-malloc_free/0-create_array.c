a34d289d593dc4d6cfe7a5a4cac470e44ea0769c
alemi <alemiherbert@gmail.com>
2023-03-13 20:50:01 +0300

    Task 0 complete

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * create_array - create an array of chars and initialize it with
+ * a specific char
+ *
+ * @size: the size of the array
+ * @c: the char
+ *
+ * Return: a pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *result;
+
+	if (!(result = malloc(size * sizeof(char))))
+		return (NULL);
+	for (i = 0; i < size; i++)
+		*(result + i) = c;
+
+	return (result);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+# 0x0B. C - Malloc, free
+In this chapter of the ALX experience, I try my hand at dynamic memory allocation in C using malloc and free
