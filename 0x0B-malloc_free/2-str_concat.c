f91404e4db7c2bec03b75a044851186941482f61
alemi <alemiherbert@gmail.com>
2023-03-13 23:51:04 +0300

    Correct test case 5

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,10 +22,19 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1 == NULL)
+			break;
 		result[i] = s1[i];
+	}
 
 	j = i;
 	for (i = 0; s2[i] != '\0'; i++, j++)
+	{
+		if (s2 == NULL)
+			break;
 		result[j] = s2[i];
+	}
+
 	return (result);
 }
