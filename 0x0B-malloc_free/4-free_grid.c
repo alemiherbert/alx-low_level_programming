56c6175136ad7baa19f8dabf8d380d56ff3ff2e4
alemi <alemiherbert@gmail.com>
2023-03-14 14:15:15 +0300

    Task 4 complete

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * free_grid - free a 2-dimensional grid
+ * @grid: the grid
+ * @height: the height of the grid
+*/
+void free_grid(int **grid, int height)
+{
+    if (grid && height)
+    {
+        while (height--)
+            free(grid[height]);
+        free(grid);
+    }
+    return;
+}
