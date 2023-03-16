410ff62813813d9b357c6a9b81eb67806c2fc998
alemi <alemiherbert@gmail.com>
2023-03-17 00:45:44 +0300

    Passing local build

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocate a memory block using malloc and free
+ * @ptr: pointer to the block of memory
+ * @old_size: the size of the old block
+ * @new_size: the size of the new block
+ *
+ * Return: pointer to the allocated block of memory, or NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_block, *temp;
+	unsigned int i, min;
+
+	temp = (char *) ptr;
+	if (!ptr)
+	{
+		new_block = malloc(new_size * sizeof(char));
+		if (!new_block)
+			return (NULL);
+	}
+	if (ptr && !new_size)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (new_size > old_size)
+	{
+		new_block = malloc(new_size * sizeof(char));
+		if (!new_block)
+			return (NULL);
+
+		min = old_size > new_size ? new_size : old_size;
+		for (i = 0; temp[i] != '\0' || i < min; i++)
+			new_block[i] = temp[i];
+		free(ptr);
+	}
+	return (new_block);
+}
