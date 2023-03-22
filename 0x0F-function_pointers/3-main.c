66beff780162bb191486049ea9176b8de64cf583
alemi <alemiherbert@gmail.com>
2023-03-23 00:45:21 +0300

    Titus's tests

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,20 +8,35 @@
  */
 int main(int argc, char **argv)
 {
-	int n1, n2, result;
-	char *opr;
-	int (*fn)(int, int);
+int num1, num2;
+	char *operator;
 
+	/* Error if arguments are not 4 with the executable */
 	if (argc != 4)
-		return (EXIT_FAILURE);
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
-	opr = argv[2];
+	num1 = atoi(argv[1]); /*if argument is string convert to number*/
+	num2 = atoi(argv[3]); /* as stated above */
+	operator = argv[2];
 
-	fn = get_op_func(opr);
+	/* Error if operator is not null or a 2d array '\*' */
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-	result = fn(n1, n2);
-	printf("%d\n", result);
-	return (EXIT_SUCCESS);
+	/* Error if user tries to divide / (47) or % (37) by 0*/
+	if ((*operator == 47 || *operator == 37) && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(operator)(num1, num2));
+
+	return (0);
 }
