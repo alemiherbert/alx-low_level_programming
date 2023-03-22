300a8ed88953148fabfd1edfabb8f7cc590c1f47
alemi <alemiherbert@gmail.com>
2023-03-23 01:00:54 +0300

    Minor bugfix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,24 +13,24 @@ int main(int argc, char **argv)
 
 	if (argc != 4)
 	{
-		puts("Error");
-		return (1);
+		printf("Error\n");
+		exit(98);
 	}
 
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[3]);
-	opr = argv[2];
+	opr= argv[2];
 
 	if (get_op_func(opr) == NULL || opr[1] != '\0')
 	{
-		puts("Error");
-		return (1);
+		printf("Error\n");
+		exit(99);
 	}
 
-	if ((*opr == '/' || *opr == '%') && n2 == 0)
+	if ((*opr== 47 || * opr== 37) && n2 == 0)
 	{
-		puts("Error");
-		return (1);
+		printf("Error\n");
+		exit(100);
 	}
 	printf("%d\n", get_op_func(opr)(n1, n2));
 	return (0);
