5e3575402a6c89ea2b8bd9ba9dcd24f7dc893ace
alemi <alemiherbert@gmail.com>
2023-03-22 21:51:40 +0300

    Task 0 complete

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#ifdef FUNCTION_POINTERS_H
+#include "function_pointers.h"
+#endif
+#include <stdio.h>
+
+/**
+ * print_name - a function that prints a name
+ * @name: the name
+ * @f: pointer to a modifier for the name
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
