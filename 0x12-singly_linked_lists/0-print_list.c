118ffe2255f3bb17708619b94380cac8d9223f60
alemi <alemiherbert@gmail.com>
2023-03-30 16:45:52 +0300

    Initial commit Complete task 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - print all the elements of a singly linked list
+ * @h: pointer to the head of the list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 1;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		printf("[%d] %s\n", h->len, (h->str == NULL) ? "[0] (nil)" : h->str);
+		h = h->next;
+		i++;
+	}
+	/* Print the last node */
+	printf("[%d] %s\n", h->len, (h->str == NULL) ? "[0] (nil)" : h->str);
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+# 0x12. C - Singly linked lists
+In this chapter of the ALX experience, I explore linked lists, their traversal, adding and removing elements
