bfa788bac92b4d0fc168312b7f45b023770c7722
Alemi Herbert <alemiherbert@gmail.com>
2023-04-05 01:33:35 +0300

    Fix linter issues

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,20 +3,21 @@
 /**
  * print_listint - print all the elements of a listint_t object
  * @h: pointer to the first node of the list
- * 
+ *
  * Return: the number of elements printed
 */
 size_t print_listint(const listint_t *h)
 {
-    size_t n = 0;
-    if (!h)
-        return (0);
+	size_t n = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        n++;
-    }
-    return (n);
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
 }
