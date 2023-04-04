80d69044a217b35b496949bda93dde8fb401f38d
Alemi Herbert <alemiherbert@gmail.com>
2023-04-05 02:34:13 +0300

    Complete task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a listint_t list
+ * @head: a pointer to a pointer to the first node
+ * Description: this function sets head the to NULL
+*/
+void free_listint2(listint_t **head)
+{
+    listint_t *aux;
+
+    while((aux = *head))
+    {
+        *head = (*head)->next;
+        free(aux);
+    }
+    *head = NULL;
+}
