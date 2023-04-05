19ff72c89dfbda07a03d4a443105b11fe6bb0479
Alemi Herbert <alemiherbert@gmail.com>
2023-04-05 04:14:41 +0300

    Handle index 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,17 +1,16 @@
 #include "lists.h"
 
-
 /**
  * insert_nodeint_at_index - insert a new node at a given position
  * @head: pointer to the pointer to the first node
  * @idx: where to insert the node
  * @n: the data to be stored in the node added
- * 
+ *
  * Return: a pointer to the new node
-*/
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 1;
+	unsigned int i = 0;
 	listint_t *_node, *aux;
 
 	_node = malloc(sizeof(listint_t));
@@ -20,9 +19,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	_node->n = n;
 
 	aux = *head;
+	if (idx == 0)
+	{
+		_node->next = *head;
+		*head = _node;
+		return (*head);
+	}
 	while (aux)
 	{
-		if (i == idx)
+		if (i == idx - 1)
 		{
 			_node->next = aux->next;
 			aux->next = _node;
