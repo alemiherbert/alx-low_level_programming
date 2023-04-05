0cc4782bbfe70aae111a8d8cc1f660076d8faaf8
Alemi Herbert <alemiherbert@gmail.com>
2023-04-05 05:40:27 +0300

    Fix linting issues

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,32 +9,32 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    unsigned int i = 0;
-    listint_t *aux, *tmp;
+	unsigned int i = 0;
+	listint_t *aux, *tmp;
 
-    if (!*head)
-        return (-1);
+	if (!*head)
+		return (-1);
 
-    aux = *head;
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        free(aux);
-        return (1);
-    }
-    while (aux)
-    {
-        if (i == index - 1)
-        {
-            tmp = aux->next;
-            if (aux->next)
-                aux->next = tmp->next;
-            else
-                aux->next = NULL;
-            free(tmp);
-        };
-        aux = aux->next;
-        i++;
-    }
-    return (1);
+	aux = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(aux);
+		return (1);
+	}
+	while (aux)
+	{
+		if (i == index - 1)
+		{
+			tmp = aux->next;
+			if (aux->next)
+				aux->next = tmp->next;
+			else
+				aux->next = NULL;
+			free(tmp);
+		};
+		aux = aux->next;
+		i++;
+	}
+	return (1);
 }
