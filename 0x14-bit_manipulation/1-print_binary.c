8ce53beaed6aa61b77496b343c828a8b4eeb41a2
Alemi Herbert <alemiherbert@gmail.com>
2023-04-08 00:00:49 +0300

    Add putchar; complete task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_binary(unsigned long int n)
+{
+    unsigned long int rem;
+    if (n == 1 || n == 0)
+    {
+        _putchar(n + '0');
+        return;
+    }
+    rem = n - ((n >> 1) << 1);
+    n >>= 1;
+    print_binary(n);
+    _putchar(rem + '0');
+}
