179dbae80212b425bfafdd6eed3e53771ea5b092
Alemi Herbert <alemiherbert@gmail.com>
2023-04-08 11:22:55 +0300

    Fix linting issues

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * get_bit - return the value of a bit at a given index
+ * @n: the number
+ * @index: the index
+ *
+ * Return: the value of the bit, or 0
+*/
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (!(index < 64))
-        return (-1);
-    if (1 << index & n)
-        return (1);
-    return (0);
+	if (!(index < 64))
+		return (-1);
+	if (1 << index & n)
+		return (1);
+	return (0);
 }
