c685617f26904fd2bdc70b5313c6013e87ae266a
Alemi Herbert <alemiherbert@gmail.com>
2023-04-10 04:32:52 +0300

    Ugly hack, hope it works!

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,6 +18,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
     
     /* Write contents from buffer to stdout */
     write(1, buf, len);
+    putchar('\n');
+    len++;
     free(buf);
     
     return (len);
