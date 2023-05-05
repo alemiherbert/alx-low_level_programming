9cda82b90d402dee3bde4dfe298e96b336339bc9
Alemi Herbert <alemiherbert@gmail.com>
2023-05-05 03:26:50 +0300

    Complete task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * print_binary - print the binary representation of a number
+ * @n: the number
+ * 
+*/
+void print_binary(unsigned long int n)
+{
+    unsigned long int remainder;
+
+    if (n == 0 || n == 1)
+    {
+        _putchar(n + '0');
+        return;
+    }
+    remainder = n - ((n >> 1) << 1);
+    n >>= 1;
+    print_binary(n);
+    _putchar(remainder + '0');
+}
