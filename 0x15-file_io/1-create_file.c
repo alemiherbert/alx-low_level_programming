86f4eaa7af778e71fc205ba4ba6f7d6d4f6a2603
Alemi Herbert <alemiherbert@gmail.com>
2023-05-09 02:12:17 +0300

    Complete task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - this function creates a file
+ * @filename: the name of the file
+ * @text_content: things to write to the file
+ *
+ * Return: 1 if success -1 if failed
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t len;
+	ssize_t nwrite;
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		return (1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	len = strlen(text_content);
+	nwrite = write(fd, text_content, len);
+	close(fd);
+
+	if (nwrite == -1)
+		return (-1);
+	return (1);
+}
