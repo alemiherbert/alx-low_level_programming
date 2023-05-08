a279820a9ca014194d6eed545ad04fad6086f8ae
Alemi Herbert <alemiherbert@gmail.com>
2023-05-09 02:30:18 +0300

    Add truncation support

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,11 +16,18 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-
-	fd = open(filename, O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR);
 	if (fd == -1)
-		return (-1);
-
+	{
+		fd = open(filename, O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+		if (fd == -1)
+			return (-1);
+	}
+	else
+	{
+		ftruncate(fd, 0);
+		return (1);
+	}
 	len = strlen(text_content);
 	nwrite = write(fd, text_content, len);
 	close(fd);
