4c036f5554bd6b5e9a8a91a4a1754b19a3e0a5e6
Alemi Herbert <alemiherbert@gmail.com>
2023-05-09 02:13:11 +0300

    Complete task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,6 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (!text_content)
-		return (1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
 	if (fd == -1)
