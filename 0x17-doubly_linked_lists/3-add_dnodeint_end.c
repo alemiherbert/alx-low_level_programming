920773e05fd27dbf980adf18aa292d8bf0ab34f3
Alemi Herbert <alemiherbert@gmail.com>
2023-08-17 23:43:29 +0300

    Bug fix

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -6,21 +6,21 @@
  * @head: pointer to the first node
  * @n: number to store in the new node
  */
-dlistint_t *add_dnodeint_end(dnodeint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dnodeint_t *new_node, *tmp;
+    dlistint_t *new_node, *tmp;
 
     if (!head)
         return (NULL);
 
-    new_node = malloc(sizeof(dnodeint_t));
+    new_node = malloc(sizeof(dlistint_t));
     if (!new_node)
         return (NULL);
 
     new_node->n = n;
     new_node->next = NULL;
 
-    if (*head = NULL)
+    if (*head == NULL)
     {
         new_node->prev = NULL;
         *head = new_node;
